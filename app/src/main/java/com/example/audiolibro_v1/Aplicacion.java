17a138edf5d321164d8b74ac18647c78d34eaf53
andres <andres97gt@gmail.com>
2018-09-27 21:13:05 -0500

    Proyecto AudioLibro

---
+++ app/src/main/java/com/example/audiolibro_v1/Aplicacion.java
@@ -0,0 +1,26 @@
+package com.example.audiolibro_v1;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+
+import java.util.Vector;
+
+public class Aplicacion extends Application {
+
+    private Vector<Libro> vectorLibros;
+    private AdaptadorLibrosFiltro adaptador;
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onCreate() {
+        vectorLibros = Libro.ejemploLibros();
+        adaptador = new AdaptadorLibrosFiltro (this, vectorLibros);
+    }
+    public AdaptadorLibrosFiltro getAdaptador() {
+        return adaptador;
+    }
+    public Vector<Libro> getVectorLibros() {
+        return vectorLibros;
+    }
+
+}
---
+++ app/src/main/java/com/example/audiolibro_v1/PreferenciasFragment.java
@@ -0,0 +1,10 @@
+package com.example.audiolibro_v1;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class PreferenciasFragment extends PreferenceFragmentCompat {
+    @Override public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
